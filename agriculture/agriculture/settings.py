3f8fa52e795a30af10ca1fe9dbdbda7567463867
rishav-67 <60477346+rishav-67@users.noreply.github.com>
2021-06-17 12:58:32 +0530

    first commit

--- agriculture/agriculture/settings.py
+++ agriculture/agriculture/settings.py
@@ -16,7 +16,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+print(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     
 ]
 
@@ -120,13 +121,14 @@ USE_L10N = True
 USE_TZ = True 
 
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR,'static') 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'static')
-]
 
+ 
 
+MEDIA_URL= '/media/'
+MEDIA_ROOT=BASE_DIR / 'media'
--- agriculture/agro/urls.py
+++ agriculture/agro/urls.py
@@ -11,4 +11,4 @@ urlpatterns = [
     path('graphscommon/',views.graphcommon,name="iskagraphco"),
     path('datas/',views.headgo,name="iskahead")
            
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
